c30afd2ea17219bf5eb4ea24e60e601b91ea7d39
junmay <bsy00133@naver.com>
2023-06-12 10:31:58 +0900

    1031

---
+++ hello/src/main/java/soccer/hello/team/Team.java
@@ -0,0 +1,30 @@
+package soccer.hello.team;
+
+public class Team {
+    private int teamId;
+    private String teamName;
+    private int leagueId;
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getLeagueId() {
+        return leagueId;
+    }
+
+    public void setLeagueId(int leagueId) {
+        this.leagueId = leagueId;
+    }
+}
