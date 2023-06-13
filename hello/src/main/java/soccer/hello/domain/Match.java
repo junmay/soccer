4ed716c03bdd0db18a45c54176ccc02df7d5214b
junmay <bsy00133@naver.com>
2023-06-13 11:04:33 +0900

    1111

--- hello/src/main/java/soccer/hello/domain/Match.java
+++ hello/src/main/java/soccer/hello/domain/Match.java
@@ -1,5 +1,21 @@
 package soccer.hello.domain;
 
+import lombok.Data;
+
+@Data
 public class Match {
-    private int
+    private int matchId;
+    private int teamId;
+    private int team2Id;
+    private String date;
+
+    @Override
+    public String toString() {
+        return "Match{" +
+                "matchId=" + matchId +
+                ", teamId='" + teamId + '\'' +
+                ", team2Id='" + team2Id + '\'' +
+                  ", date='"+ date +'\''+
+                '}';
+    }
 }
