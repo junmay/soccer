5a32d0787600a2e046bac5ff87149be786eef3aa
sim5132004 <129469224+sim5132004@users.noreply.github.com>
2023-06-13 16:50:34 +0900

    006/12 11:45

--- hello/src/main/java/soccer/hello/web/PlayerController.java
+++ hello/src/main/java/soccer/hello/web/PlayerController.java
@@ -3,10 +3,14 @@ package soccer.hello.web;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import soccer.hello.domain.Player;
 import soccer.hello.service.PlayerServi;
+
+import java.util.List;
 
 //@RestController
 @Controller
@@ -32,7 +36,7 @@ public class PlayerController {
     }
 
 
-    @PostMapping
+//    @PostMapping
     public String findPlayer(){
         int playerId=0;
         Player player = playerService.findPlayer(playerId).get();
@@ -40,6 +44,20 @@ public class PlayerController {
 
         return "redircet:/p";
     }
+
+    @GetMapping("searchp")
+    public String searchPlayer(){
+        return "searchplayer";
+    }
+
+    @GetMapping("/searchplayer")
+    public String playerBy(@ModelAttribute("playerSearch")Player player, Model model) {
+
+        Player playerBy = playerService.findPlayer(player.getPlayerId()).get();
+        model.addAttribute("playerBy", playerBy);
+        return "searchplayer";
+    }
+
 
 
 }
