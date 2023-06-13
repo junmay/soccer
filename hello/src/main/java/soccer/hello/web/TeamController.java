80f05638f1041c67b086973d8d316ecd15e8279a
haedong Kim <ekslatjstods@naver.com>
2023-06-13 10:07:53 +0900

    11

---
+++ hello/src/main/java/soccer/hello/web/TeamController.java
@@ -0,0 +1,32 @@
+package soccer.hello.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import soccer.hello.service.PlayerServi;
+import soccer.hello.service.TeamService;
+
+//@RestController
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class TeamController {
+    private final TeamService teamService;
+
+//    public LeagueController(LeagueService leagueService) {
+//        this.leagueService = leagueService;
+//    }
+
+//    @GetMapping("/leagues")
+//    public List<League> getAllLeagues() {
+//        return leagueService.getAllLeagues();
+//    }
+
+    @GetMapping("/teamInfo")
+    public String teamInfo(){
+        return "/teamInfo.html";
+    }
+
+
+}
