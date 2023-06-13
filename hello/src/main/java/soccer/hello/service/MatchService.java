3c0e5b0bcdf9123ea18b3825a81364ad19170713
junmay <bsy00133@naver.com>
2023-06-13 13:18:20 +0900

    1111

--- hello/src/main/java/soccer/hello/service/MatchService.java
+++ hello/src/main/java/soccer/hello/service/MatchService.java
@@ -1,6 +1,7 @@
 package soccer.hello.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import soccer.hello.Repository.LeagueRepository;
 import soccer.hello.Repository.MatchRepository;
@@ -10,6 +11,7 @@ import soccer.hello.domain.Match;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MatchService implements MatchServi{
 //    private final LeagueMapper leagueMapper;
@@ -18,6 +20,9 @@ public class MatchService implements MatchServi{
 
 
     public List<Match> getAllMatches(){
+
+        log.info("안녕하세요 겟 올 매치 입니다. "
+        );
        return matchRepository.getAllMatches();
     }
 
--- hello/src/main/java/soccer/hello/web/MatchController.java
+++ hello/src/main/java/soccer/hello/web/MatchController.java
@@ -2,16 +2,25 @@ package soccer.hello.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import soccer.hello.domain.Match;
 import soccer.hello.service.LeagueServi;
 import soccer.hello.service.MatchServi;
+
+import java.util.List;
 
 //@RestController
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MatchController {
+    @Autowired
     private final MatchServi matchService;
 
 //    public LeagueController(LeagueService leagueService) {
@@ -23,12 +32,13 @@ public class MatchController {
 //        return leagueService.getAllLeagues();
 //    }
 
-    @GetMapping("/match")
-    public String getAll(){
-        log.info("dddd");
-        log.info(matchService.getAllMatches()+"");
-        return "index";
+
+    @GetMapping ("/matches")
+    public String getAllMatches(Model model) {
+        List<Match> matches = matchService.getAllMatches();
+        log.info(matches+" ");
+
+        model.addAttribute("matches",matches);
+        return "main_1";
     }
-
-
 }
